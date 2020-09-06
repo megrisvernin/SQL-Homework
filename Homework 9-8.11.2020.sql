aaacb6f583632e7c991365e879ca193ad743a445
Thomas Pelletier <megrisvernin@gmail.com>
2020-09-06 15:33:52 -0400

    "Renaming Homework 9"

--- Homework 9-8.11.2020.sql
+++ Homework 9-8.11.2020.sql
@@ -1,94 +1,116 @@
-CREATE TABLE [Course] (
-  [CourseID] Int,
-  [Coursename] varchar(30),
-  [CourseCredits] Int,
-  PRIMARY KEY ([CourseID])
-);
-
-CREATE TABLE [Major] (
-  [MajorID] Int,
-  [Name] varchar(30),
-  [CreditsToGrad] Int,
-  PRIMARY KEY ([MajorID])
-);
-
-CREATE TABLE [Student] (
-  [StudentID] Int,
-  [MajorID] Int,
-  [FirstName] varchar(30),
-  [LastName] varchar(30),
-  [Age] Int,
-  [HasFinancialAid] Bit,
-  [Email] varchar(30),
-  PRIMARY KEY ([StudentID])
-);
-
-CREATE TABLE [Enrollment] (
-  [ScheduledClassID] Int,
-  [StudentID] Int Not NULL,
-  [CourseID] Int Not Null,
-  [SemesteriD] Int Not NULL,
-  [ClassLocationID] Int,
-  [DateEnrolled] Date Default getDATE(),
-  [GradePoint] Int,
-  PRIMARY KEY ([ScheduledClassID])
-);
-
-CREATE TABLE [Semester] (
-  [SemesterID] Int,
-  [Name] varchar(30),
-  [StartDate] Date,
-  [EndDate] Date,
-  PRIMARY KEY ([SemesterID])
-);
-
-CREATE TABLE [ClassLocation] (
-  [ClassLocationID] Int,
-  [Address] varchar(100),
-  [RoomNumber] Int,
-  [DaysoftheWeek] varchar(7),
-  [Startime] Time,
-  [DurationTimes] Int,
-  [StudentCapacity] Int ,
-  PRIMARY KEY ([ClassLocationID])
-);
-
-
-Drop table ClassLocation,Semester,Enrollment,Student,Major,Course
-
----Homework 9
----Use Update statement to add constraints, add relevaqnts from before and any new ones that seem relevant, 
----add foreign keys according to diagram
----Andrew do Enrollment and Course
+--Question 1
+--Use Update statement to ADD CONSTRAINTs, add relevaqnts from before and any new ones that seem relevant, 
+--add foreign keys according to diagram
+--Andrew do Enrollment and Course
 --Thomas Fill out ClassLocation and Semmester
---Both do Major and Student, format values to match new table
---Homework
-
---Return Filname, FilmeRunTime
-
---WHERE SUBQUERY Returns
-
+--Both do Major and Student, format VALUES to match new table"
+--Question 2
+--Return Filname, FilmeRunTime WHERE SUBQUERY Returns 
 --Run Time between 90 and 120 Minutes
---
---WHat films were released in the same year as King Kong, use a subquery to do this
---Note to self King Kong is 3 movies so you want to do filename Like 'King Kong'
---Use = and In on subquery
---
---Do the same but filmname Serenity
----Use = and In on subquery
+--Using Wherelookup
+Select
+filmname
+,FilmRunTimeMinutes
+From tblfilm
+WHERE FilmRunTimeMinutes BETWEEN 90 AND 120
+ORDER BY Filmname
+
+--Use Subquery
+USE Movies
+SELECT
+	Filmname
+	,FilmRunTimeMinutes
+FROM 
+	tblFilm
+WHERE
+	FilmRunTimeMinutes IN
+		(SELECT
+			FilmRunTimeMinutes
+		FROM
+			tblFilm
+		WHERE 
+			FilmRunTimeMinutes BETWEEN 90 AND 120)
+
+--Question 3
+--What films were released in the same year as around the world in 80 days, use a subquery to do this
+--Note to self King Kong is 3 movies 
+--IN
+
+SELECT
+	FilmName
+	,FilmReleaseDate
+	,DATENAME(Year,FilmReleaseDate) AS YEAR
+FROM
+	tblFilm
+WHERE 
+	DATENAME(Year,FilmReleaseDate) = --IN works for queries returning multiple items, = is only good for a subquery looking up one item
+		(SELECT 
+			DATENAME(Year,FilmReleaseDate)
+		FROM
+			tblfilm
+		WHERE 
+			Filmname = 'Around the World in 80 days')
+
+--Question 3
+--What films were released in the same year as King Kong, use a subquery to do this
+--Note to self King Kong is 3 movies 
+--=
+SELECT
+	FilmName
+	,FilmReleaseDate
+	,DATENAME(YEAR,FilmReleaseDate) AS [Year]
+FROM
+	tblFilm
+WHERE
+	DATENAME(YEAR,FilmReleaseDate) IN --IN works for queries returning multiple items, = is only good for a subquery looking up one item
+		(SELECT 
+			DATENAME(YEAR,FilmReleaseDate)
+		FROM
+			tblfilm
+		WHERE
+			Filmname = 'Around the World in 80 days')
+
+--Question 4
+--What films were released in the same year as Serenity, use a subquery to do this
+--IN
+
+SELECT
+	FilmName
+	,FilmReleaseDate
+	,DATENAME(Year,FilmReleaseDate) AS Year
+FROM
+	tblFilm
+WHERE
+	DATENAME(Year,FilmReleaseDate) IN
+		(SELECT 
+			DATENAME(Year,FilmReleaseDate)
+		FROM
+			tblfilm
+		WHERE
+			Filmname = 'Serenity')
+ORDER BY Filmname
+
+--Question 3
+--What films were released in the same year as Serenity, use a subquery to do this
+--Note to self King Kong is 3 movies 
+--=
+SELECT
+	FilmName
+	,FilmReleaseDate
+	,DATENAME(YEAR,FilmReleaseDate) AS year
+FROM
+	tblFilm
+WHERE
+	DATENAME(YEAR,FilmReleaseDate) =
+		(SELECT 
+			DATENAME(YEAR,FilmReleaseDate)
+		FROM
+			tblfilm
+		WHERE 
+			Filmname = 'Serenity')
 
 --Homework 10
 --Examples of
--- subquery returning a set of values
--- subquery returning a single value
+-- subquery returning a set of VALUES
+-- subquery returning a single value"
 
-
-
-
-
-
-
-
-
-
-And we can both do Major and Student
